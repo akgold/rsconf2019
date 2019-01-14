b8d0d298e06a1cf552dd998d8a4620e5924ffc66
Alex Gold <gold_alex@bah.com>
2019-01-14 11:16:54 -0500

    Add plots

--- helper_funcs.R
+++ helper_funcs.R
@@ -6,11 +6,15 @@ bar_plot <- function(df, ylab) {
     geom_label(aes(label = lab, y = 0.5), family = "Cabin-Regular") +
     scale_y_continuous(labels = scales::percent) +
     ylab(ylab) +
-    ggthemes::theme_few() +
+    theme_rs() +
     theme(axis.text.x = element_blank(),
           axis.title.x = element_blank(),
-          axis.ticks.x = element_blank(),
-          text = element_text(family = "Cabin-Regular"))
+          axis.ticks.x = element_blank())
+}
+
+theme_rs <- function() {
+    ggthemes::theme_few() +
+    theme(text = element_text(family = "Cabin-Regular"))
 }
 
 make_bar_dat <- function(y, labs) {
@@ -19,3 +23,16 @@ make_bar_dat <- function(y, labs) {
          lab = labs  %>%
            stringr::str_wrap(20))
 }
+
+
+# ggplot(mtcars, aes(factor(cyl), mpg)) +
+#   geom_boxplot() +
+#   # Here comes the gganimate code
+#   transition_states(
+#     gear,
+#     transition_length = 2,
+#     state_length = 1
+#   ) +
+#   enter_fade() +
+#   exit_shrink() +
+#   ease_aes('sine-in-out')
