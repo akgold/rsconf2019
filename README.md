56f70b16e8a8a115f2fd643651fc766134e9a7c1
Alex Gold <gold_alex@bah.com>
2019-01-16 15:06:18 -0600

    Add near-final

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # RStudio Conf 2019 E-Poster
 
 This presentation of tips on upgrading to R was presented at RStudio::conf(2019).
+
+Thanks to github pages, the slideshow is running at https://akgold.github.io/rsconf2019
--- config.R
+++ config.R
@@ -1,14 +1,17 @@
 library(magrittr)
 
-tips <- c(
-  skills = "excitement >> skills",
-  fail = "be ready to fail",
-  csvs = "meet your data where it lives",
-  package = "team writes a package",
-  git = "git: takes time, worth it.",
-  markdown = "use Rmarkdown",
-  invisible = "don't start with TensorFlow"
+tips1 <- c(
+  skills = "Excitement >> Skills",
+  fail = "Failure's Coming, Get Ready",
+  csvs = "You are the Data Engineer your Team Needs",
+  package = "It Takes a Team to Write a Package",
+  git = "Git: Sometimes the Right Thing isn't the Easiest",
+  markdown = "Have you met my Friend RMarkdown?",
+  invisible = "Don't Get Too Excited Just Yet"
 )
+
+tips <- sprintf("%s. %s", seq(length(tips1)), tips1)
+names(tips) <- names(tips1)
 
 knitr::knit_expand(file = here::here("pres_skell.Rmd"), tips = tips) %>%
   writeLines(here::here("pres.Rmd"))
